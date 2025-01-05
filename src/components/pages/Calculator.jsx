6d7459b8e1e4c641a4210739bea6d5c8bc11c6c9
[mishra gaurav] <vishwajeetsingh@tecraki.io>
2025-01-05 20:16:43 +0530

    add new code calculator

--- src/components/pages/Calculator.jsx
+++ src/components/pages/Calculator.jsx
@@ -1,61 +1,102 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Calculator = () => {
-    const [input , setInput] = useState("");
+  const [input, setInput] = useState("");
 
-    const handleClick = (value) => {
-        setInput(input + value);
-    };
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
 
-    const calculateResult = () => {
-        try {
-            const result = eval(input);
-            setInput(String(result));
-        } catch (error) {
-            setInput("Error");
-        }
+  const calculateResult = () => {
+    try {
+      const result = eval(input);
+      setInput(String(result));
+    } catch (error) {
+      setInput("Error");
     }
+  }
 
-
-    const clearInput = () => {
-        setInput("");
-
-    }
+  const clearInput = () => {
+    setInput("");
+  }
 
   return (
-  <>
-  <div className="container calculator text-bg-danger p-4 text-center align-items-center rounded" 
-  style={{width:"25%", marginTop:"4%"}}>
-    <div className="display">
-        <h1>Calculator</h1>
-      <input type="text" value={input} disabled />
-    </div>
-    <div className='button mt-4'>
-       <button onClick={() => handleClick("1")} type='button' className='button-color'>1</button> 
-       <button onClick={() => handleClick("2")} type='button' className='button-color'>2</button> 
-       <button onClick={() => handleClick("3")} type='button' className='button-color'>3</button> 
-       <button onClick={() => handleClick("+")} type='button' className='button-color'>+</button> 
-
-       <button onClick={() => handleClick("4")} type='button' className='button-color'>4</button> 
-       <button onClick={() => handleClick("5")} type='button' className='button-color'>5</button> 
-       <button onClick={() => handleClick("6")} type='button' className='button-color'>6</button> 
-       <button onClick={() => handleClick("-")} type='button' className='button-color'>-</button> 
-
-       <button onClick={() => handleClick("7")} type='button' className='button-color'>7</button> 
-       <button onClick={() => handleClick("8")} type='button' className='button-color'>8</button> 
-       <button onClick={() => handleClick("9")} type='button' className='button-color'>9</button> 
-       <button onClick={() => handleClick("*")} type='button' className='button-color'>*</button> 
-
-       <button onClick={() => handleClick("0")} type='button' className='button-color'>0</button> 
-       <button onClick={clearInput} type='button' className='button-color'>C</button> 
-       <button onClick={() => handleClick("/")} type='button' className='button-color'>/</button> 
-       <button onClick={calculateResult} type='button' className='button-color'>=</button> 
-    </div>
-  </div>
-  </>
+    <>
+      <div className="container" style={{marginTop: "8%"}}>
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="card text-bg-danger">
+              <div className="card-body">
+                <h2 className="card-title text-center">Calculator</h2>
+                <input type="text" value={input} disabled className="form-control" />
+                <div className="mt-4">
+                  <div className="row">
+                    <div className="col-3">
+                      <button onClick={() => handleClick("1")} type='button' className='btn btn-light w-100'>1</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("2")} type='button' className='btn btn-light w-100'>2</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("3")} type='button' className='btn btn-light w-100'>3</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("+")} type='button' className='btn btn-light w-100'>+</button>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-3">
+                      <button onClick={() => handleClick("4")} type='button' className='btn btn-light w-100'>4</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("5")} type='button' className='btn btn-light w-100'>5</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("6")} type='button' className='btn btn-light w-100'>6</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("-")} type='button' className='btn btn-light w-100'>-</button>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-3">
+                      <button onClick={() => handleClick("7")} type='button' className='btn btn-light w-100'>7</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("8")} type='button' className='btn btn-light w-100'>8</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("9")} type='button' className='btn btn-light w-100'>9</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("*")} type='button' className='btn btn-light w-100'>*</button>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-6">
+                      <button onClick={() => handleClick("0")} type='button' className='btn btn-light w-100'>0</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={clearInput} type='button' className='btn btn-light w-100'>C</button>
+                    </div>
+                    <div className="col-3">
+                      <button onClick={() => handleClick("/")} type='button' className='btn btn-light w-100'>/</button>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-12">
+                      <button onClick={calculateResult} type='button' className='btn btn-light w-100'>=</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
 export default Calculator;
-
 
